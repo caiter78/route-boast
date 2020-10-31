2815f4cd4934f634b1a28e9cc2b87fa71b1f46e9
Andrii Martyniuk <andrii_martyniuk@hotmail.com>
2020-10-31 20:52:59 +0200

    Init web api

---
+++ Entity/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Entity
+{
+    public class User
+    {
+        private long id;
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public User(long id, string firstName, string lastName, string email)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+        }
+    }
+}
---
+++ RouteBoast/Controllers/v1/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteBoast.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetAll))]
+        public ActionResult GetAll(ApiVersion version)
+        {
+            return new OkResult();
+        }
+        
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult GetOne(ApiVersion version, int id)
+        {
+            return new OkResult();
+        }
+    }
+}
